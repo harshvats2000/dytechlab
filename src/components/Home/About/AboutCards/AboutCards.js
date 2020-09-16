9a594ea6617110538534d0db2b59b98cc8a3eb4e
Abhishek Ranjan <abhishekranjan0505@gmail.com>
2020-09-16 09:59:22 +0530

    some improvements

--- src/components/Home/About/AboutCards/AboutCards.js
+++ src/components/Home/About/AboutCards/AboutCards.js
@@ -1,26 +1,31 @@
 import React from "react";
 import classes from "./AboutCards.module.css";
 import AboutCard from "../AboutCard/AboutCard.js";
-// import TradingLogo from "./Logo/TradingLogo.js";
+import TradingLogo from "../Logo/TradingLogo.js";
 
 const aboutCards = () => {
   return (
     <div className={classes.AboutCards}>
-      <AboutCard heading="Trading" button="Learn More">
+      <AboutCard
+        heading="Trading and Technology"
+        button="Learn More"
+        logo={<TradingLogo />}
+      >
         Our strategies are founded on in-depth research, custom technology and
         careful risk assessment.
       </AboutCard>
-      <AboutCard heading="Trading" button="Learn More">
-        Our strategies are founded on in-depth research, custom technology and
-        careful risk assessment.
+
+      <AboutCard
+        heading="Other ventures"
+        button="Learn More"
+        logo={<TradingLogo />}
+      >
+        As the markets have evolved, so have we, growing to include
+        cryptoassets, venture capital and real estate.
       </AboutCard>
-      <AboutCard heading="Trading" button="Learn More">
-        Our strategies are founded on in-depth research, custom technology and
-        careful risk assessment.
-      </AboutCard>
-      <AboutCard heading="Trading" button="Learn More">
-        Our strategies are founded on in-depth research, custom technology and
-        careful risk assessment.
+      <AboutCard heading="Careers" button="Learn More" logo={<TradingLogo />}>
+        Our people drive our success. We hire the best, provide the right tools
+        and reward outstanding results.
       </AboutCard>
     </div>
   );
