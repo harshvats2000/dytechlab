ad23e53f782b8d9d519f3129db4625c700492ee7
Abhishek Ranjan <abhishekranjan0505@gmail.com>
2020-09-17 23:34:32 +0530

    about us page developed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import Home from './container/Home/home';
+import React from "react";
+import "./App.css";
+import AboutUs from "./container/AboutUs/AboutUs";
 
 function App() {
   return (
-    <div className='App'>
-      <Home />
+    <div className="App">
+      <AboutUs />
     </div>
   );
 }
---
+++ src/components/AboutUs/OurApproach/OurApproach.js
@@ -0,0 +1,110 @@
+import React from "react";
+import classes from "./OurApproach.module.css";
+
+const OurApproach = () => {
+  return (
+    <div className={classes.Root}>
+      <h4>Our Approach</h4>
+      <p>
+        We characterize our trading activity as following three general
+        approaches —
+      </p>
+      <ul>
+        <li>
+          <p>
+            Liquidity providing: making markets, buying and selling products to
+            provide liquidity for market participants, with execution on a
+            trading floor, over the phone and electronically.
+          </p>
+        </li>
+
+        <li>
+          <p>
+            Risk taking:&nbsp;doing our homework, formulating a view and putting
+            on a position.&nbsp;Some positions are held for a few days, some for
+            a few months or even years.&nbsp;
+          </p>
+        </li>
+
+        <li>
+          <p>
+            Latency sensitive trading: combining research and risk management
+            with superior technology and execution on our own extensive global
+            radio frequency (RF) network
+          </p>
+        </li>
+      </ul>
+      <p>
+        Trading in any asset class might include just one of these or may bring
+        together all three; it all depends on what we see as the right strategy.
+        Our knowledge of the markets, pricing and modeling skills, robust risk
+        management, and application of technology work together to create
+        liquidity, improve market efficiency and capture opportunities.
+      </p>
+      <h5>People</h5>
+      <p>
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book. It has survived not only five centuries, but
+        also the leap into electronic typesetting, remaining essentially
+        unchanged. It was popularised in the 1960s with the release of Letraset
+        sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem
+        Ipsum.
+      </p>
+      <h6>Education Background</h6>
+      <p>
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book. It has survived not only five centuries, but
+        also the leap into electronic typesetting, remaining essentially
+        unchanged. It was popularised in the 1960s with the release of Letraset
+        sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem
+        Ipsum.
+      </p>
+      <h6>Cultural Background</h6>
+      <p>
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book. It has survived not only five centuries, but
+        also the leap into electronic typesetting, remaining essentially
+        unchanged. It was popularised in the 1960s with the release of Letraset
+        sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem
+        Ipsum.
+      </p>
+      <h5>Technology</h5>
+      <p>
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book. It has survived not only five centuries, but
+        also the leap into electronic typesetting, remaining essentially
+        unchanged. It was popularised in the 1960s with the release of Letraset
+        sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem
+        Ipsum.
+      </p>
+      <h6>In-House System</h6>
+      <p>
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book.
+      </p>
+      <h6>ACM Winner Team</h6>
+      <p>
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book.
+      </p>
+      <h6>Techniques used (MlL, NLP, BIG DATA)</h6>
+      <p>
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book.
+      </p>
+    </div>
+  );
+};
+
+export default OurApproach;
