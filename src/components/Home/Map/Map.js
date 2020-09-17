776b0ac5129b547a0f284bae8a19e4c58e607e5e
Harsh Vats <vatsvatsharsh2000@gmail.com>
2020-09-17 16:00:00 +0530

    changed about headings

--- src/components/Home/Map/Map.js
+++ src/components/Home/Map/Map.js
@@ -1,16 +1,16 @@
-import React from "react";
-import Logo from "../../DrwLogo/Logo";
-import classes from "./Map.module.css";
-import mapImg from "../../../assets/map.png";
+import React from 'react';
+import Logo from '../../DrwLogo/Logo';
+import classes from './Map.module.css';
+import mapImg from '../../../assets/map.png';
 
 const map = () => {
   return (
-    <div className={classes.Root} style={{ backgroundImage: `url(${mapImg})` }}>
+    <div className={classes.Root}>
       <p className={classes.Content}>
-        <Logo fill="#233b56" />{" "}
-        <span style={{ marginLeft: "5px" }}>
-          We empower a team of exceptional individuals to identify and capture
-          trading and investment opportunities globally.
+        <Logo fill='#233b56' />{' '}
+        <span style={{ marginLeft: '5px' }}>
+          We empower a team of exceptional individuals to identify and capture trading and
+          investment opportunities globally.
         </span>
       </p>
     </div>
