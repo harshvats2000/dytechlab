0b6383ddcdb936375493f0226023745f9b0cdf55
Harsh Vats <vatsvatsharsh2000@gmail.com>
2020-09-18 21:58:26 +0530

    bugs fixed

--- src/components/Home/Map/Map.js
+++ src/components/Home/Map/Map.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import classes from './Map.module.css';
-import mapImg from '../../../assets/map.png';
 
 const map = () => {
   return (
     <div className={classes.Root}>
       <p className={classes.Content}>
-        {/* <Logo fill='#233b56' />{' '} */}
-        {/* <img src='/images/logo.jpeg' /> */}
-        <span style={{ marginLeft: '5px' }}>
+        <span>
           We empower a team of exceptional individuals to identify and capture trading and
           investment opportunities globally.
         </span>
