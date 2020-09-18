9e253ab920e1a08f3203d7c0e2d5561904eb0eb4
Harsh Vats <vatsvatsharsh2000@gmail.com>
2020-09-18 22:36:31 +0530

    image size issue resolved

--- src/container/AboutUs/AboutUs.js
+++ src/container/AboutUs/AboutUs.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import PageHeader from '../../components/AboutUs/PageHeader/PageHeader';
-// import Intro from "../../components/AboutUs/Intro/Intro";
-// import History from "../../components/AboutUs/History/History";
-// import OurApproach from "../../components/AboutUs/OurApproach/OurApproach";
 import classes from './AboutUs.module.css';
-// import Trading from "../../components/AboutUs/Trading/Trading";
 
 class AboutUs extends Component {
   render() {
