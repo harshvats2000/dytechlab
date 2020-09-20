aad7342489e2bb8cf7c372e5594fe3268a40d46a
Harsh Vats <vatsvatsharsh2000@gmail.com>
2020-09-20 19:55:33 +0530

    cultures page changes

--- src/container/Culture/Culture.js
+++ src/container/Culture/Culture.js
@@ -23,7 +23,7 @@ export default function Culture() {
 
         <div className={classes.body}>
           <div className={classes.intro}>
-            <h1 className='heading'>Our Culture</h1>
+            {/* <h1 className='heading'>Our Culture</h1> */}
             <p className={`para`}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt soluta perferendis
               quas voluptas ad illum non dicta deleniti adipisci tenetur?
@@ -92,47 +92,47 @@ export default function Culture() {
               throughout our careers.
             </h1>
             <div className={classes.fun}>
-              <div className={classes.fun_card}>
+              <div className={`${classes.fun_card} ${classes.fun_card1}`}>
                 <div className={classes.fun_card_title}>Movie Night</div>
                 <img src='/images/about/1.jpeg' />
               </div>
 
-              <div className={classes.fun_card}>
+              <div className={`${classes.fun_card} ${classes.fun_card2}`}>
                 <div className={classes.fun_card_title}>pantry food</div>
                 <img src='/images/about/1.jpeg' />
               </div>
 
-              <div className={classes.fun_card}>
+              <div className={`${classes.fun_card} ${classes.fun_card3}`}>
                 <div className={classes.fun_card_title}>gym</div>
                 <img src='/images/about/1.jpeg' />
               </div>
 
-              <div className={classes.fun_card}>
+              <div className={`${classes.fun_card} ${classes.fun_card4}`}>
                 <div className={classes.fun_card_title}>friday snack</div>
                 <img src='/images/about/1.jpeg' />
               </div>
 
-              <div className={classes.fun_card}>
+              <div className={`${classes.fun_card} ${classes.fun_card5}`}>
                 <div className={classes.fun_card_title}>Game/poker</div>
                 <img src='/images/about/1.jpeg' />
               </div>
 
-              <div className={classes.fun_card}>
+              <div className={`${classes.fun_card} ${classes.fun_card6}`}>
                 <div className={classes.fun_card_title}>jogging</div>
                 <img src='/images/about/1.jpeg' />
               </div>
 
-              <div className={classes.fun_card}>
+              <div className={`${classes.fun_card} ${classes.fun_card7}`}>
                 <div className={classes.fun_card_title}>bbq/outing</div>
                 <img src='/images/about/1.jpeg' />
               </div>
 
-              <div className={classes.fun_card}>
+              <div className={`${classes.fun_card} ${classes.fun_card8}`}>
                 <div className={classes.fun_card_title}>annual treats</div>
                 <img src='/images/about/1.jpeg' />
               </div>
 
-              <div className={classes.fun_card}>
+              <div className={`${classes.fun_card} ${classes.fun_card9}`}>
                 <div className={classes.fun_card_title}>Marathon</div>
                 <img src='/images/about/1.jpeg' />
               </div>
